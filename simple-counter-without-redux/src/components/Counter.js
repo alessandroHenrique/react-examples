56fd407f0939a6a2a185216994cb9871d27f08db
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-06 10:10:49 -0300

    Change Counter from stateless component to class component.
    Now that I'm sure that I'll need state to handle the counter value,
    I'm changing the Counter component to Class based component.

--- simple-counter-without-redux/src/components/Counter.js
+++ simple-counter-without-redux/src/components/Counter.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Counter = () => (
-  <div>
-    <h1>Counter</h1>
-    <button>+1</button>
-    <button>-1</button>
-    <button>Reset</button>
-  </div>
-)
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <button>+1</button>
+        <button>-1</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
