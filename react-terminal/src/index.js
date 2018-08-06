080d5ded266b04003d9c83cad002fc2b4776398c
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-06 13:23:42 -0300

    Import MainPage to be rendered in the index.js.

--- react-terminal/src/index.js
+++ react-terminal/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import MainPage from './components';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainPage />, document.getElementById('root'));
 registerServiceWorker();
