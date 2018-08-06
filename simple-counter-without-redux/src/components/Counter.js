0b85295b014ea41ac417a01d0bd7630e093b0c46
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-06 10:26:25 -0300

    Add handle methods for the buttons.
    For the +1 button I added the handleIncrementButton method.
    For the -1 button I added the handleDecrementButton method.
    For the Reset Button I added the handleResetButton method.

--- simple-counter-without-redux/src/components/Counter.js
+++ simple-counter-without-redux/src/components/Counter.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
 class Counter extends React.Component {
+  state = {
+    counter: 0
+  }
+
+  handleIncrementButton = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
+  }
+
+  handleDecrementButton = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter - 1
+    }));
+  }
+
+  handleResetButton = () => {
+    this.setState((prevState) => ({
+      counter: 0
+    }));
+  }
+
   render() {
     return (
       <div>
