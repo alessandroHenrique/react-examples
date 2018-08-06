8dc404f4f11b3da806c86ef867582edfb0f81938
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-06 10:05:41 -0300

    Import the Counter component to be displayed.
    I removed the App component which is the default by create-react-app
    and added my Counter component.

--- simple-counter-without-redux/src/index.js
+++ simple-counter-without-redux/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Counter from './components/Counter';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
