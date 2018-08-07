ef0a8fd02c7691948d77863a10fd5e1321f0a047
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-07 09:38:28 -0300

    Import index.css to use local styles.

--- react-terminal/src/index.js
+++ react-terminal/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import MainPage from './components';
 import registerServiceWorker from './registerServiceWorker';
 
