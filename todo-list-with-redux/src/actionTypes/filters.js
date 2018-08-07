74edda408302dab9d0b2cd7f1db955c3394b9da7
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-07 10:27:10 -0300

    Add action types for the filters.
    It consists of only one action:
    SET_VISIBILITY_FILTER - Set the current visibility filter for the todos. The default value is SHOW_ALL

---
+++ todo-list-with-redux/src/actionTypes/filters.js
@@ -0,0 +1,6 @@
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const visibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
