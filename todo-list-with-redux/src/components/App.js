079f51170530ff99b57bbea7c6ccd8bca30b7545
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-08 16:18:27 -0300

    Display the addTodoFormContainer in the main page.

--- todo-list-with-redux/src/components/App.js
+++ todo-list-with-redux/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import AddTodoFormContainer from './AddTodoFormContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>It works!</h1>
+        <AddTodoFormContainer />
       </div>
     );
   }
