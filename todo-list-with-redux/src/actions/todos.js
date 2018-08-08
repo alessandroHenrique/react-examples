e2cf2040495092b1ff659e68eeb6f1ac8c810ed0
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-08 09:20:08 -0300

    Replace the removeTodo action for a toggleTodo.

--- todo-list-with-redux/src/actions/todos.js
+++ todo-list-with-redux/src/actions/todos.js
@@ -1,11 +1,11 @@
-import { ADD_TODO, REMOVE_TODO } from '../actionTypes/todos';
+import { ADD_TODO, TOGGLE_TODO } from '../actionTypes/todos';
 
 export const addTodo = (text) => ({
   type: ADD_TODO,
   text
 });
 
-export const removeTodo = (id) => ({
-  type: REMOVE_TODO,
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
   id
 });
