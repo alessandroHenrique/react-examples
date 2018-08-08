a1f648a68ce4de6e80e9b68a935cb9f9316dff19
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-08 10:31:25 -0300

    Add id for the created todos.
    I used the uuid lib to provide unique ids for each new todo.

--- todo-list-with-redux/src/actions/todos.js
+++ todo-list-with-redux/src/actions/todos.js
@@ -1,8 +1,10 @@
 import { ADD_TODO, TOGGLE_TODO } from '../actionTypes/todos';
+import uuid from 'uuid';
 
 export const addTodo = (text) => ({
   type: ADD_TODO,
-  text
+  text,
+  id: uuid()
 });
 
 export const toggleTodo = (id) => ({
