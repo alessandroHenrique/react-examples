09635db8a72baa64f04f20e117def85fdc558e7f
Alessandro <alessandrohenriqueho@gmail.com>
2018-08-09 14:46:39 -0300

    Put the Todos list element in the main App file

--- todo-list-with-redux/src/components/App.js
+++ todo-list-with-redux/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AddTodoFormContainer from './AddTodoFormContainer';
+import Todos from './Todos';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>It works!</h1>
         <AddTodoFormContainer />
+        <Todos />
       </div>
     );
   }
